7912f2c1648ab2e52f31e17ee429b3a7e5e94626
水靈 <z23320785@gmail.com>
2024-12-13 10:01:20 +0800

    update .md note

--- Database/Migrations/microsoft-entityFrameworkCore-sqlServer.ps1
+++ Database/Migrations/microsoft-entityFrameworkCore-sqlServer.ps1
@@ -1,6 +1,6 @@
 # 設定資料庫連接字串和其他參數
 # Set the database connection string and other parameters
-$connectionString = "Server=tcp:35.236.189.154,1433;Database=MociiDashboard;UID=louis005;PWD=louis005;Connect Timeout=2;Max Pool Size=300;Min Pool Size=1;"
+$connectionString = "Server=tcp:{ip},1433;Database={name};UID={};PWD={};Connect Timeout=2;Max Pool Size=300;Min Pool Size=1;"
 $provider = "Microsoft.EntityFrameworkCore.SqlServer"
 $outputDir = "Models"
 $contextName = "ApplicationDbContext"
